486ddddd6346abfd58baa36e4a77acf60a347a16
sencin <asunayuk34@gmail.com>
2026-04-27 08:30:10 +0800

    removed footer

--- src/App.vue
+++ src/App.vue
@@ -120,10 +120,5 @@ const features = [
       </a>
     </section>
 
-    <!-- FOOTER -->
-    <footer class="text-center py-8 text-gray-500 dark:text-gray-400 text-sm">
-      © 2026 BoardTracker. All rights reserved.
-    </footer>
-
   </div>
 </template>
